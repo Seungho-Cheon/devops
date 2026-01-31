8d952d43285bc19d6361df6d6a7e8ce2135f47ab
cheonsho <jongroinf@naver.com>
2026-01-31 11:24:07 +0900

    Change to Web Server

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-# main.py
+from flask import Flask
 
-import requests
+app = Flask(__name__)
 
-print("Hello, Dockerized Python!")
+@app.route('/')
+def hello():
+    return "<h1>Hello, DevOps! 🚀</h1><p>This is my first web server running in Docker.</p>"
+
+if __name__ == '__main__':
+    # 0.0.0.0은 '누구나 접속 가능'하게 연다는 뜻입니다.
+    app.run(host='0.0.0.0', port=5000)
